2052a68cf69daa669997bed311aa4ce0b400b7cb
손창우 <ckddn9496@gmail.com>
2018-10-30 00:27:49 +0900

    전체적인 보안강화(데이터 전송에 모든 정보를 전송하지 않게함) parent.reqLoc: 관리하는 유저의 정보 요청

--- Nodejs/lib/login.js
+++ Nodejs/lib/login.js
@@ -16,26 +16,26 @@ exports.signIn = function(req, res) {
     console.log(sql);
     db.query(sql, inputData.id, function(err, datas, fields) {
         console.log(datas);
-        // console.log(fields);
         if (err) {
         console.log('Error!');
-        res.send('Error!');
+        res.send('에러로 인하여 로그인에 실패하였습니다. 잠시후 다시 시도해주세요.');
         } else if (datas[0] == null) {
         console.log('unknown ID');
-        res.send('unknown ID');
-        } //  Error or do not exist corresponding ID...
-        var opts = {password : inputData.pw, salt : datas[0].salt}  //  login pw, db salt
-        hasher(opts, function(err, pass, salt, hash) {
-        if (hash == datas[0].pw) {
-            var info = {
-            no : datas[0].no,
-            id : datas[0].id
-            }
-            res.send(info);
+        res.send('등록되지 않은 아이디 입니다.');
         } else {
-            console.log('wrong Password');
-            res.send('wrong Password');
+            var opts = {password : inputData.pw, salt : datas[0].salt}  //  login pw, db salt
+            hasher(opts, function(err, pass, salt, hash) {
+            if (hash == datas[0].pw) {
+                var info = {
+                no : datas[0].no,
+                id : datas[0].id
+                }
+                res.send(info);
+            } else {
+                console.log('wrong Password');
+                res.send('비밀번호가 일치하지 않습니다.');
+            }
+            });
         }
-        });
     }); 
 }
