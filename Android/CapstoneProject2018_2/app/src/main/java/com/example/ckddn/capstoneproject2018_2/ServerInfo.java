64c5200c0569bde50feaa748633c67b227fea045
손창우 <ckddn9496@gmail.com>
2018-11-12 19:54:00 +0900

    new Server IPAddress

--- Android/CapstoneProject2018_2/app/src/main/java/com/example/ckddn/capstoneproject2018_2/ServerInfo.java
+++ Android/CapstoneProject2018_2/app/src/main/java/com/example/ckddn/capstoneproject2018_2/ServerInfo.java
@@ -3,7 +3,7 @@ package com.example.ckddn.capstoneproject2018_2;
 
 public interface ServerInfo {
     /*  setting server ip address   */
-//    public static final String ipAddress = "35.221.104.89:5555";   //  Google Cloud Platform 외부 고정 ip
-    public static final String ipAddress = "192.168.0.13:5555";   //  Google Cloud Platform 외부 고정 ip
+    public static final String ipAddress = "35.243.118.35:5555";   //  Google Cloud Platform 외부 고정 ip
+//    public static final String ipAddress = "192.168.0.13:5555";   //  Google Cloud Platform 외부 고정 ip
 
 }
