c2677ce7d9f910458e8605e57fafcaba0de2e383
DESKTOP-MQCSJKO\insu0 <insu0929@naver.com>
2018-11-13 16:51:32 +0900

    POI ver 1.0 추가된 전체 프로젝트 버그 수정 예정

--- Android/CapstoneProject2018_2/app/src/main/java/com/example/ckddn/capstoneproject2018_2/SetPathActivity.java
+++ Android/CapstoneProject2018_2/app/src/main/java/com/example/ckddn/capstoneproject2018_2/SetPathActivity.java
@@ -3,11 +3,17 @@ package com.example.ckddn.capstoneproject2018_2;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.skt.Tmap.TMapData;
+import com.skt.Tmap.TMapPOIItem;
 import com.skt.Tmap.TMapView;
 
 import org.json.JSONException;
@@ -23,87 +29,76 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class SetPathActivity extends AppCompatActivity {
     private String uno, pno;
+    private String keyword;
+    TMapView tMapView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_path);
 
-        uno = getIntent().getStringExtra("uno");
-        pno = getIntent().getStringExtra("pno");
-        Button send = (Button) findViewById(R.id.send_path);
-        send.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new SendDestTask().execute("http://" + ServerInfo.ipAddress + "/parent/setDestination");
+        final TMapData tMapData = new TMapData();
+        final ListView listView;
+        final PoiListViewAdapter adapter;
+
+        adapter = new PoiListViewAdapter();
+
+        listView = (ListView)findViewById(R.id.poi_listView);
+        listView.setAdapter(adapter);
+
+        final EditText poiName_editText;
+        poiName_editText = (EditText)findViewById(R.id.des_edit_text);
+        Button searchBtn;
+        searchBtn = (Button)findViewById(R.id.searchBtn);
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    keyword = poiName_editText.getText().toString();
+                                    if(!TextUtils.isEmpty(keyword)){
+                                        tMapData.findAllPOI(keyword, new TMapData.FindAllPOIListenerCallback() {
+                                            @Override
+                                            public void onFindAllPOI(final ArrayList<TMapPOIItem> arrayList) {
+                                                runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    for(TMapPOIItem poi : arrayList){
+                                        adapter.notifyDataSetChanged();
+                                        adapter.putSearchResults(new POI(poi));
+                                    }
+                                    if(arrayList.size()>0){
+                                        TMapPOIItem poi = arrayList.get(0);
+                                    }
+                                    }
+                            });
+                            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                                    Toast.makeText(SetPathActivity.this, arrayList.get(position).getPOIPoint().toString(),Toast.LENGTH_LONG).show();
+
+                                }
+                            });
+
+
+
+
+                        }
+                    });
+                }
             }
         });
-    }
+
+        tMapView = new TMapView(this);
+        tMapView.setSKTMapApiKey( "85bd1e2c-d3c1-4bbf-93ca-e1f3abbc5788\n" );
+
+        uno = getIntent().getStringExtra("uno");
+        pno = getIntent().getStringExtra("pno");
 
 
-    public class SendDestTask extends AsyncTask<String, String, String> {
-        String TAG = "SendDestTask>>>";
-        @Override
-        protected String doInBackground(String... strings) {
-            try {   //  json accumulate
-                JSONObject destInfo = new JSONObject();
-                destInfo.accumulate("uno", uno);
-                destInfo.accumulate("pno", pno);
 
-                /* longitude와 latitude는 보호자가 POI검색으로 받아온 목적지의 위도경도이다 */
-                destInfo.accumulate("longitude", 126.9707345);
-                destInfo.accumulate("latitude", 37.4818587);
-
-                Log.d(TAG, "doInBackground: create json");
-                HttpURLConnection conn = null;
-                BufferedReader reader = null;
-                try {   //  for HttpURLConnection
-                    URL url = new URL(strings[0]);
-                    conn = (HttpURLConnection) url.openConnection();
-                    conn.setRequestMethod("POST");  //  POST방식
-                    conn.setRequestProperty("Cache-Control", "no-cache");        // 컨트롤 캐쉬 설정(?)
-                    conn.setRequestProperty("Content-Type", "application/json"); // json형식 전달
-                    conn.setRequestProperty("Accept", "application/text");       // text형식 수신
-                    conn.setRequestProperty("Accept", "application/json");       // json형식 수신
-                    conn.setDoOutput(true); //  OutputStream으로 POST데이터 전송
-                    conn.setDoInput(true);  //  InputStream으로 서버로부터 응답 전달받음
-                    conn.connect();
-
-                    OutputStream outputStream = conn.getOutputStream();
-                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
-                    //  버퍼생성
-                    writer.write(destInfo.toString());
-                    writer.flush();
-                    writer.close();
-                    //  send Sign In Info to Server...
-                    InputStream stream = conn.getInputStream();
-                    reader = new BufferedReader(new InputStreamReader(stream));
-                    StringBuffer buffer = new StringBuffer();
-                    String line = "";
-                    while((line = reader.readLine()) != null) {
-                        //  readLine : string or null(if end of data...)
-                        buffer.append(line);
-                        Log.d(TAG, "doInBackground: readLine, " + line);
-                    }
-                    return buffer.toString();
-                } catch (MalformedURLException e) {
-                    //  이상한 URL일 때
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
-        }
     }
 }
