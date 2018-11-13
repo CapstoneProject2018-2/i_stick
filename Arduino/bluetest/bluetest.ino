8931129cf0651bd8d943856eeed7a3c514559a47
LJH\lee <qltlfdl1030@naver.com>
2018-11-14 01:34:00 +0900

    아두이노 네비게이션

---
+++ Arduino/bluetest/bluetest.ino
@@ -0,0 +1,51 @@
+
+#include <Servo.h>
+#include <SoftwareSerial.h>
+int bluetoothTx = 2;
+int bluetoothRx = 3;
+Servo servo;
+int value=90;
+long toSend;
+SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
+ 
+void setup()
+{
+  servo.attach(4);
+  servo.write(value);
+  //Setup usb serial connection to computer
+  Serial.begin(9600);
+  bluetooth.begin(9600);
+}
+ 
+ 
+void loop()
+{ 
+  //Read from bluetooth and write to usb serial 
+  if(bluetooth.available())
+  {
+    toSend = 0;
+    toSend = bluetooth.parseInt();
+    
+    if(toS end==13) {
+      value=0;
+      servo.write(value);
+      delay(3000);
+      servo.write(90);
+    } else if (toSend==12) { //left
+      value=180;
+      servo.write(value);
+      delay(3000);
+      servo.write(90);
+    } else if(toSend==11) {
+      value=90;
+      servo.write(value);
+      delay(3000);
+      servo.write(90);
+    }
+      
+    Serial.println(toSend);
+    
+  }
+  
+  
+}
