d519bba2f5952a0985412a86c7061b216c9e8a6f
손창우 <ckddn9496@gmail.com>
2018-11-20 15:18:04 +0900

    npm fcm-node

--- Nodejs/lib/register.js
+++ Nodejs/lib/register.js
@@ -1,25 +1,26 @@
 var db = require('./db')
 const bkfd2Password = require('pbkdf2-password'); //  for hash the passwd
 var hasher = bkfd2Password();                     //  hash func
+
 exports.signUp = function (req, res) {
   console.log('/register');
   //  register data from android and INSERT in Database
   const inputData = req.body;
   console.log(inputData);
   var sql = '';
-  var id = inputData.id;
-  var pw = inputData.pw;
-  var name = inputData.name;
-  var mobile = inputData.mobile;
-
+  const id = inputData.id;
+  const pw = inputData.pw;
+  const name = inputData.name;
+  const mobile = inputData.mobile;
+  const token = inputData.token;
   if (inputData.type === 0) {  //  user
-    sql = 'INSERT INTO user (id, pw, salt, name, mobile) VALUES (?,?,?,?,?);';
+    sql = 'INSERT INTO user (id, pw, salt, name, mobile, token) VALUES (?,?,?,?,?,?);';
   } else if (inputData.type === 1) {  //  parent
-    sql = 'INSERT INTO parent (id, pw, salt, name, mobile) VALUES (?,?,?,?,?);';
+    sql = 'INSERT INTO parent (id, pw, salt, name, mobile, token) VALUES (?,?,?,?,?,?);';
   }
   var opts = { password: pw };
   hasher(opts, function (err, pass, salt, hash) {
-    db.query(sql, [id, hash, salt, name, mobile], function (err, data) {
+    db.query(sql, [id, hash, salt, name, mobile, token], function (err, data) {
       if (err) {
         console.log(err);
         res.send('에러로 인해 아이디 등록이 실패하였습니다. 잠시후 다시 시도해주세요.')
@@ -37,9 +38,9 @@ exports.checkId = function (req, res) {
   console.log(inputData);
   var sql = '';
   if (inputData.type === 0) {  //  user
-    sql = 'SELECT * FROM user WHERE id=?';
+    sql = 'SELECT id FROM user WHERE id=?';
   } else if (inputData.type === 1) {  //  parent
-    sql = 'SELECT * FROM parent WHERE id=?';
+    sql = 'SELECT id FROM parent WHERE id=?';
   }
   db.query(sql, inputData.id, function (err, data) {
     if (err) {
