d5a6d087970591db2ab9ead1028d2b38110b23c2
DESKTOP-MQCSJKO\insu0 <insu0929@naver.com>
2018-12-02 17:40:22 +0900

    디버그 모드 맵 마커 종류 오류 수정 ->dotflag flow에 따라서 잘 찍어줌

--- Android/CapstoneProject2018_2/app/src/main/java/com/example/ckddn/capstoneproject2018_2/Oblu/PointDrawer.java
+++ Android/CapstoneProject2018_2/app/src/main/java/com/example/ckddn/capstoneproject2018_2/Oblu/PointDrawer.java
@@ -32,26 +32,26 @@ public class PointDrawer {
         if(dotflag == 1){ //dr point를 그려줄 때
             TMapMarkerItem item1 = new TMapMarkerItem();
             Bitmap bitmap = null;
-            bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.dr_point);
+            bitmap = BitmapFactory.decodeResource(context.getResources(),R.drawable.blue_point_resize);
 
             item1.setTMapPoint(pos);
             item1.setName("" + System.currentTimeMillis());
             item1.setVisible(item1.VISIBLE);
             item1.setIcon(bitmap);
-            bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.dr_point);
+            bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.blue_point_resize);
             tMapView.addMarkerItem("" +itemID, item1);
 
 
         }else if(dotflag ==0){ //gps point를 그려줄 때4
             TMapMarkerItem item2 = new TMapMarkerItem();
             Bitmap bitmap = null;
-            bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.dr_point);
+            bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.gps_point);
 
             item2.setTMapPoint(pos);
             item2.setName("" + System.currentTimeMillis());
             item2.setVisible(item2.VISIBLE);
             item2.setIcon(bitmap);
-            bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.dr_point);
+            bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.gps_point);
 
             tMapView.addMarkerItem("" +itemID, item2);
         }
